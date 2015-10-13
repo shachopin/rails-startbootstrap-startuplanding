d9c58547ba0e9ca86d23226d299668d95259c5b2
Dawei Dai <shachopin@gmail.com>
2015-10-13 10:29:16 -0700

    asdasd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ RailsStartbootstrap1::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.serve_static_assets = true
+  #config.serve_static_assets = true
 end
